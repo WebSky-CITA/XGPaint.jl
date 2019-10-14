c7f6cdf0e0eaa6d1a1bfe21c89536e8072e890ca
xzackli <xzackli@gmail.com>
2019-10-14 19:06:20 -0400

    basic tests

--- src/XGPaint.jl
+++ src/XGPaint.jl
@@ -1,5 +1,8 @@
 module XGPaint
 
 greet() = print("Hello World!")
+f(x) = 2 * x
+
+export f
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,9 @@
 using XGPaint
 using Test
 
+@test f(1) == 2
+
 @testset "XGPaint.jl" begin
     # Write your own tests here.
+    @test f(1) == 2
 end
