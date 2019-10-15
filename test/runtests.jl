19ae3a6cbff917c4360cda1342dee104bc1dbc1a
xzackli <xzackli@gmail.com>
2019-10-15 09:59:17 -0400

    added test for viero

--- test/runtests.jl
+++ test/runtests.jl
@@ -3,5 +3,7 @@ using Test
 
 @testset "XGPaint.jl" begin
     # Write your own tests here.
-    @test f(1) == 2
+    # @test f(1) == 2
+    model = CIBModel{Float32}()
+    @test model.min_mass ≈ 1.0f12
 end
