e1de19cc18d4a4d22b0d839335b00408b8b07a62
xzackli <xzackli@gmail.com>
2019-10-15 11:55:28 -0400

    concentration factor functions

--- src/XGPaint.jl
+++ src/XGPaint.jl
@@ -73,10 +73,91 @@ function build_r2z_interpolator(min_z::T, max_z::T, cosmo; n_bins=1000) where T
     return r2z
 end
 
+function jiang_shmf(m, M_halo, model)
+    dndm = (((model.jiang_gamma_1*((m/M_halo)^model.jiang_alpha_1))+
+             (model.jiang_gamma_2*((m/M_halo)^model.jiang_alpha_2)))*
+             (exp(-(model.jiang_beta_2)*((m/M_halo)^model.jiang_zeta))))
+    return dndm
+end
+
+"""
+Build a linear interpolation function which maps log(M_h) to N_sat.
+"""
+function build_shang_interpolator(
+    min_log_M::T, max_log_M::T, model::CIBModel; n_bin=1000) where T
+
+    x_m = LinRange(min_log_M, max_log_M, 1000)
+    N_sat_i = zero(x_m)
+
+    function integrand_m(lm, lM_halo)
+        # lm is natural log of m
+        m = exp(lm)
+        M_halo = exp(lM_halo)
+        dns_dm = jiang_shmf(m, M_halo, model)
+        return dns_dm
+    end
+
+    for i in 1:size(x_m,1)
+        N_sat_i[i], err = quadgk( t->integrand_m(t, x_m[i]),
+                log(model.shang_Msmin), x_m[i], rtol=1e-8)
+        N_sat_i[i] = convert(T, max(0.0, N_sat_i[i]))
+    end
+
+    return LinearInterpolation(x_m, N_sat_i)
+end
+
+# concentration factor from Duffy et al. 2008
+mz2c(m::Float64, z::Float64, cosmo) = 7.85 * (
+    m / (2.0e12/cosmo.h))^(-0.081) / (1.0+z)^0.71
+mz2c(m::Float32, z::Float32, cosmo) = 7.85f0 * (
+    m / (2.0f12/cosmo.h))^(-0.081f0) / (1.0f0+z)^0.71f0
+
+#
+# function m2r(m, par)
+#     return (3.0f0 * m / fourpif / par["rhocrit"])^(1.0f0 / 3.0f0)
+# end
+#
+# function f_nfw(x::T) where T
+#     return log1p(x) - x/(one(T)+x)
+# end
+# g(x,c,m) = m - f_nfw(x)/f_nfw(c)
+
 
 export CIBModel
 
 
+
+
+# function fill_halovars!(
+#         x, y, z, # inputs
+#         redshift_result, dist_result) # outputs
+#     """
+#     This function computes distance and redshift in parallel.
+#     """
+#
+#     N_halos = size(x,1)
+#     @sync @distributed for i = 1:N_halos
+#         dist_result[i] = sqrt(x[i]^2 + y[i]^2 + z[i]^2)
+#         redshift_result[i] = r2z(dist_result[i])
+#     end
+# end
+#
+# function hod_shang(cen_result, sat_result, sat_bar_result,
+#         halo_mass::SharedArray, par)
+#     # computes shang HOD and generates a Poisson draw
+#     min_lm = log(minimum(halo_mass))
+#     max_lm = log(maximum(halo_mass))
+#     logM_to_N = build_shang_interp(min_lm, max_lm, par)
+#     N_halos = size(halo_mass,1)
+#
+#     @sync @distributed for i = 1:N_halos
+#         cen_result[i] = 1
+#
+#         sat_bar_result[i] = logM_to_N(log(halo_mass[i]))
+#         sat_result[i] = pois_rand(convert(Float64, sat_bar_result[i]))
+#     end
+#
+# end
 
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,15 +2,27 @@ using XGPaint
 using Cosmology
 using Test
 
-# background evolution is within 1e-7 between Julia and Python implementations
-rtol = 1e-6
+# relative background evolutions differ by 1e-3 between Julia and Python
+rtol = 1e-3
 cosmo = cosmology(h=0.7, OmegaM=0.25)
 r2z = XGPaint.build_r2z_interpolator(0.0f0, 4.5f0, cosmo)
+model = CIBModel{Float32}()
+hod_shang = XGPaint.build_shang_interpolator( log(1.0f13), log(1.0f15), m)
 
 @testset "halo2fluxmap" begin
     # These tests come from computing specific numbers from the Python xgpaint
-    model = CIBModel{Float32}()
+
     @test model.shang_Mpeak ≈ 10^12.3  # check default is viero as docs say
-    @test isapprox(r2z(3407.6934323399264f0), 1.0, rtol=rtol)
+
+    # python: h2fm.utils.r2z(3407.0f0)
+    @test isapprox(r2z(3407.0f0), 0.9996, rtol=rtol)
+
+    # python: h2fm.utils.jiang_shmf(0.5e12, 1e13)
+    @test XGPaint.jiang_shmf(0.5f12, 1.0f13, model) ≈ 2.5335820678141165
+
+    # python: h2fm.hod.hod_shang(np.array([1e13, 1e14, 1e15]))
+    @test hod_shang(log(1.0f13)) ≈ 9.78325852
+    @test hod_shang(log(1.0f14)) ≈ 54.21666623
+    @test hod_shang(log(1.0f15)) ≈ 336.55409152
 
 end
