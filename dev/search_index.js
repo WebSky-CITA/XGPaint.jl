43fe7e17b11807b5b359fb6d9b598c33d6e22842
zeptodoctor <44736852+zeptodoctor@users.noreply.github.com>
2019-10-15 00:07:45 +0000

    build based on 7b74290

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#XGPaint.jl-1","page":"Home","title":"XGPaint.jl","text":"","category":"section"},{"location":"#","page":"Home","title":"Home","text":"","category":"page"},{"location":"#","page":"Home","title":"Home","text":"Modules = [XGPaint]","category":"page"}]
+}
