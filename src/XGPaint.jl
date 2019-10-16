3e515743d4e40d33c14b91b0d9f135f4b9073b8b
xzackli <xzackli@gmail.com>
2019-10-15 21:02:17 -0400

    added many of the CIB functions

--- src/XGPaint.jl
+++ src/XGPaint.jl
@@ -59,6 +59,41 @@ Base.@kwdef struct CIBModel{T}
 end
 
 """
+Construct a cosmology with typing. The type of the output will the type of
+`h` and `OmegaM`. The types of `h` and `OmegaM` must match.
+"""
+function get_cosmology(;h::T=0.69,
+                   Neff=3.04,
+                   OmegaK=0.0,
+                   OmegaM::T=0.29,
+                   OmegaR=nothing,
+                   Tcmb=2.7255,
+                   w0=-1,
+                   wa=0) where T
+
+    if OmegaR === nothing
+        OmegaG = 4.48131e-7*Tcmb^4/h^2
+        OmegaN = Neff*OmegaG*(7/8)*(4/11)^(4/3)
+        OmegaR = OmegaG + OmegaN
+    end
+
+    OmegaL = 1 - OmegaK - OmegaM - OmegaR
+
+    if !(w0 == -1 && wa == 0)
+        return Cosmology.WCDM{T}(h, OmegaK, OmegaL, OmegaM, OmegaR, w0, wa)
+    end
+
+    if OmegaK < 0
+        return Cosmology.ClosedLCDM{T}(h, OmegaK, OmegaL, OmegaM, OmegaR)
+    elseif OmegaK > 0
+        return Cosmology.OpenLCDM{T}(h, OmegaK, OmegaL, OmegaM, OmegaR)
+    else
+        return Cosmology.FlatLCDM{T}(h, OmegaL, OmegaM, OmegaR)
+    end
+end
+
+
+"""
 Construct a fast r2z linear interpolator.
 """
 function build_r2z_interpolator(min_z::T, max_z::T, cosmo; n_bins=1000) where T
@@ -106,22 +141,92 @@ function build_shang_interpolator(
     return LinearInterpolation(x_m, N_sat_i)
 end
 
-# concentration factor from Duffy et al. 2008
-mz2c(m::Float64, z::Float64, cosmo) = 7.85 * (
-    m / (2.0e12/cosmo.h))^(-0.081) / (1.0+z)^0.71
-mz2c(m::Float32, z::Float32, cosmo) = 7.85f0 * (
-    m / (2.0f12/cosmo.h))^(-0.081f0) / (1.0f0+z)^0.71f0
+"""
+Compute concentration factor from Duffy et al. 2008.
+"""
+function mz2c(m::T, z::T, cosmo::Cosmology.FlatLCDM{T}) where T
+    return T(7.85) * (m / (T(2.0e12)/cosmo.h))^(T(-0.081)) / (T(1.0)+z)^T(0.71)
+end
 
-#
-# function m2r(m, par)
-#     return (3.0f0 * m / fourpif / par["rhocrit"])^(1.0f0 / 3.0f0)
-# end
-#
-# function f_nfw(x::T) where T
-#     return log1p(x) - x/(one(T)+x)
-# end
-# g(x,c,m) = m - f_nfw(x)/f_nfw(c)
+"""
+Convert virial mass to virial radius.
+"""
+function m2r(m::T, cosmo::Cosmology.FlatLCDM{T}) where T
+    rho = T(2.78e11) * cosmo.Ω_m * cosmo.h^2
+    return (T(3.0/(4π)) * m / rho)^T(1.0/3.0)
+end
 
+
+function f_nfw(x::T) where T
+    return log1p(x) - x/(one(T)+x)
+end
+g(x, c, m) = m - f_nfw(x)/f_nfw(c)
+
+function solve_r(c,lnm)
+    m = exp(lnm)
+    return find_zero( x -> g(x,c,m) , (0.0,100.0), Bisection()) / c
+end
+
+"""
+Generates an interpolator r(c, lnm)
+
+Generate a LinearInterpolation object that turns concentration
+and ln(M_halo) into satellite radius.
+"""
+function build_c_lnm2r_interp(;
+    cmin::T=1f-3, cmax::T=25.0f0, mmin::T=-7.1f0, mmax::T=0.0f0,
+    nbin=100) where T
+    # these defaults imply a minimum fractional mass of exp(-7)
+
+    cs  = LinRange(cmin,cmax,nbin)
+    ms  = LinRange(mmin,mmax,nbin)
+    r = [solve_r(c_,m_) for c_ in cs, m_ in ms]
+
+    return LinearInterpolation((T.(cs), T.(ms)), T.(r))
+end
+
+random_phi(::Type{Float32}) = rand(Float32) * Float32(2π)
+random_theta(::Type{Float32}) = acos(2.0f0*rand(Float32)-1.0f0)
+random_phi(t::DataType) = rand(t) * t(2π)
+random_theta(t::DataType) = acos( t(2.0)*rand(t)-one(t))
+
+
+function sigma_cen(m::T, model::CIBModel) where T
+    return (exp( -(log10(m) - log10(model.shang_Mpeak))^2 /
+        (T(2)*model.shang_sigmaM) ) * m) / sqrt(T(2π) * model.shang_sigmaM)
+end
+
+function nu2theta(nu::T, z::T, model::CIBModel) where T
+    phys_h = T(6.62606957e-27)   # erg.s
+    phys_k = T(1.3806488e-16)    # erg/K
+    Td = model.shang_Td * (one(T)+z)^model.shang_alpha
+    xnu = phys_h * nu / phys_k / Td
+    return xnu^(T(4) + model.shang_beta) / expm1(xnu) / nu / model.shang_I0
+end
+
+"""
+<L_sat> interpolation values
+"""
+function integrand_L(lm,lM_halo, model::CIBModel)
+    m = exp(lm)
+    return sigma_cen(m, model) * jiang_shmf(m, exp(lM_halo), model)
+end
+
+
+"""
+Build a linear interpolator that takes in ln(M_halo) and returns sigma.
+"""
+function build_sigma_sat_ln(min_ln_M::T, max_ln_M::T, model;
+    n_bin=1000) where T
+    x = LinRange(min_ln_M, max_ln_M, n_bin)
+    L_mean = zero(x)
+
+    for i in 1:n_bin
+        L_mean[i], err = quadgk( t->integrand_L(t, x[i]),
+                log(shang_Mmin), x[i], rtol=1e-6)
+    end
+    return LinearInterpolation(T.(x), T.(L_mean), extrapolation_bc=zero(T))
+end
 
 export CIBModel
 
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,20 +2,22 @@ using XGPaint
 using Cosmology
 using Test
 
-# relative background evolutions differ by 1e-3 between Julia and Python
+# relative background evolutions differ by 1e-3 between Julia and Python 2
 rtol = 1e-3
-cosmo = cosmology(h=0.7, OmegaM=0.25)
+cosmo = XGPaint.get_cosmology(h=0.7f0, OmegaM=0.25f0)
+model = XGPaint.CIBModel{Float32}()
 r2z = XGPaint.build_r2z_interpolator(0.0f0, 4.5f0, cosmo)
-model = CIBModel{Float32}()
 hod_shang = XGPaint.build_shang_interpolator(log(1.0f13), log(1.0f15), model)
+clnm2r = XGPaint.build_c_lnm2r_interp(nbin=30)
 
 @testset "halo2fluxmap" begin
-    # These tests come from computing specific numbers from the Python xgpaint
+    # These tests come from computing examples in the Python xgpaint
 
     @test model.shang_Mpeak ≈ 10^12.3  # check default is viero as docs say
 
     # python: h2fm.utils.r2z(3407.0f0)
-    @test isapprox(r2z(3407.0f0), 0.9996, rtol=rtol)
+    @test isapprox(
+        r2z(3407.0f0), 0.9996, rtol=rtol)
 
     # python: h2fm.utils.jiang_shmf(0.5e12, 1e13)
     @test XGPaint.jiang_shmf(0.5f12, 1.0f13, model) ≈ 2.5335820678141165
@@ -25,4 +27,38 @@ hod_shang = XGPaint.build_shang_interpolator(log(1.0f13), log(1.0f15), model)
     @test hod_shang(log(1.0f14)) ≈ 54.21666623
     @test hod_shang(log(1.0f15)) ≈ 336.55409152
 
+    # python: h2fm.utils.mz2c(1e13, 4.0)
+    @test isapprox(
+        XGPaint.mz2c(1.0f13, 4.0f0, cosmo),
+        2.262202312880881, rtol=rtol)
+
+    # h2fm.utils.m2r(1e13)
+    @test isapprox(
+        XGPaint.m2r(1.0f13, cosmo),
+        4.12328640425709, rtol=rtol)
+
+    # h2fm.hod.clnm2r(np.array([1e-3, 25.0]), np.log(0.2))
+    @test isapprox(
+        clnm2r(1f-3 / 0.999f0, log(0.2f0)),
+        0.44787404, rtol=rtol)
+    @test isapprox(
+        clnm2r(25.0f0 / 1.001f0, log(0.2f0)),
+        0.085837, rtol=rtol)
+
+    @test XGPaint.random_phi(Float32) <= 2π
+    @test XGPaint.random_theta(Float32) <= π
+
+    # h2fm.fluxmodel.sigma_cen(1e13)
+    @test isapprox(XGPaint.sigma_cen(1.0f13, model),
+        3218663770378.3066, rtol=rtol)
+
+    # h2fm.fluxmodel.nu2theta(150e9, 0.5), h2fm.fluxmodel.nu2theta(1.5e6, 1.0)
+    @test isapprox(XGPaint.nu2theta(150e9, 0.5, model),
+        6.5705729824589e-16, rtol=rtol)
+    @test isapprox(XGPaint.nu2theta(1.5e6, 1.0, model),
+        5.945023942373837e-34, rtol=rtol)
+
+    # h2fm.fluxmodel.integrand_L(30, 32)
+    @test isapprox(XGPaint.integrand_L(30.0f0, 32.0f0, model),
+        3929486334377.691, rtol=rtol)
 end
