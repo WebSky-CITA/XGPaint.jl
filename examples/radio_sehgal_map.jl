4b483894d9823f1272ce0efa43359a9697542b0b
xzackli <xzackli@gmail.com>
2019-12-02 12:38:18 -0500

    adjusted data location

--- examples/radio_sehgal_map.jl
+++ examples/radio_sehgal_map.jl
@@ -2,7 +2,7 @@ using XGPaint
 using Healpix
 
 halo_pos, halo_mass = read_halo_catalog_hdf5(
-    "/home/zequnl/websky_halos-light.hdf5")
+    "/home/zequnl/Data/websky_halos-light.hdf5")
 ## Load halos from HDF5 files, establish a CIB model and cosmology
 cosmo = get_cosmology(h=0.7f0, OmegaM=0.25f0)
 radio_model = Radio_Sehgal2009{Float32}(a_0=-1)
@@ -17,6 +17,6 @@ end;
 @time begin
     m = Map{Float64,RingOrder}(radio_model.nside)
     paint!(m, 143f9, radio_model, sources)
-    Healpix.saveToFITS(m, "/media/data/radio143.fits")
+    Healpix.saveToFITS(m, "!/media/data/radio143.fits")
 end
 ##
