ccb8e07bb3a5ec836b66ee604ef69b7e30e86a3f
Lucas-Kuhn <84886844+Lucas-Kuhn@users.noreply.github.com>
2023-10-18 20:43:31 -0700

    Update XGPaint.jl

--- src/XGPaint.jl
+++ src/XGPaint.jl
@@ -12,24 +12,25 @@ using Healpix
 using PhysicalConstants
 using Pixell
 using Healpix: checkPixelRing
-using SpecialFunctions
 using JLD2, FileIO
 
 import ThreadsX
 import Distributions
 import DataInterpolations  # used for uneven spaced interpolators
 
+using DelimitedFiles
+
 include("./util.jl")
 include("./model.jl")
 include("./profiles.jl")
+include("./profiles_rsz.jl")
+include("./profiles_szp.jl")
 include("./cib.jl")
-include("./co_broadband.jl")
-include("./lrg.jl")
 include("./radio.jl")
 
 export get_cosmology, read_halo_catalog_hdf5, sort_halo_catalog
-export Radio_Sehgal2009, CIB_Planck2013, CIB_Scarfy, CO_CROWNED, LRG_Yuan23
-export paint!, generate_sources, process_sources, profile_grid, profile_paint!, profileworkspace
-export build_interpolator, Battaglia16ThermalSZProfile, Battaglia16RelativisticSZProfile
+export Radio_Sehgal2009, CIB_Planck2013
+export paint!, generate_sources, profile_grid, profile_paint!, profileworkspace, paint_szp!, profile_grid_szp, profile_paint_szp!, paint_rsz!, profile_grid_rsz, profile_paint_rsz!
+export build_interpolator, Battaglia16ThermalSZProfile, Battaglia16RelativisticSZProfile, build_interpolator_szp, build_interpolator_rsz
 
 end # module
