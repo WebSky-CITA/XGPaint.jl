95bd4529d179e7a9219ccd095fab5c624f004928
xzackli <xzackli@gmail.com>
2023-11-09 16:19:22 -0800

    add back scarfy utilities

--- src/XGPaint.jl
+++ src/XGPaint.jl
@@ -12,6 +12,7 @@ using Healpix
 using PhysicalConstants
 using Pixell
 using Healpix: checkPixelRing
+using SpecialFunctions
 using JLD2, FileIO
 
 import ThreadsX
@@ -26,11 +27,13 @@ include("./profiles.jl")
 include("./profiles_rsz.jl")
 include("./profiles_szp.jl")
 include("./cib.jl")
+include("./co_broadband.jl")
+include("./lrg.jl")
 include("./radio.jl")
 
 export get_cosmology, read_halo_catalog_hdf5, sort_halo_catalog
-export Radio_Sehgal2009, CIB_Planck2013
-export paint!, generate_sources, profile_grid, profile_paint!, profileworkspace, paint_szp!, profile_grid_szp, profile_paint_szp!, paint_rsz!, profile_grid_rsz, profile_paint_rsz!
+export Radio_Sehgal2009, CIB_Planck2013, CIB_Scarfy, CO_CROWNED, LRG_Yuan23
+export paint!, generate_sources, process_sources, profile_grid, profile_paint!, profileworkspace, paint_szp!, profile_grid_szp, profile_paint_szp!, paint_rsz!, profile_grid_rsz, profile_paint_rsz!
 export build_interpolator, Battaglia16ThermalSZProfile, Battaglia16RelativisticSZProfile, build_interpolator_szp, build_interpolator_rsz
 
 end # module
